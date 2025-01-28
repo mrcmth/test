e6ff9f7f3125f5929eced604c2f3173e50974bb4
Marc MATHIEU <marc.mathieu43@gmail.com>
2025-01-28 20:43:12 +0000

    Ajout du fichier code.py

---
+++ code.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+
+print("Hello ! Il est {}.".format(datetime.now().strftime("%H:%M:%S")))from datetime import datetime
+
+print("Hello ! Il est {}.".format(datetime.now().strftime("%H:%M:%S")))
