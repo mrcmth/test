2e4537111e1e3ac1bdc68e6ffd53f5d6861a4c1d
Marc MATHIEU <marc.mathieu43@gmail.com>
2025-01-28 21:18:44 +0000

    nouvelle branche

--- code.py
+++ code.py
@@ -3,3 +3,4 @@ from datetime import datetime
 print("Hello ! Il est {}.".format(datetime.now().strftime("%H:%M:%S")))from datetime import datetime
 
 print("Hello ! Il est {}.".format(datetime.now().strftime("%H:%M:%S")))
+print("commit")
